adfeb33df769cd1eb16735ad72eaab282b8936be
cultist <75032462+dairycultist@users.noreply.github.com>
2026-03-25 18:36:11 -0400

    Remove guestbook

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// sudo node index.js
+// nohup sudo node index.js &
 
 const HOMEPAGE_FILEPATH = "./homepage.html";
 const GUESTBOOK_FILEPATH = "./guestbook.txt";
@@ -17,38 +17,10 @@ createServer(options, (req, res) => {
 
 	console.log("\x1b[90m" + req.method + " " + req.url + "\x1b[0m");
 
-	let guestbookContent = "";
-
-	if (fs.existsSync(GUESTBOOK_FILEPATH))
-		guestbookContent = fs.readFileSync(GUESTBOOK_FILEPATH, "utf-8");
-
 	if (req.method == "GET" || req.method == "HEAD") {
 
-		replyWithHomepage(res, guestbookContent);
-
-	} else if (req.method == "POST" && req.url == "/guestbook") {
-
-		let body = "";
-
-		req.on("data", chunk => body += chunk.toString());
-
-		req.on("end", () => {
-
-			let parts = body.substring(10).split("\r\nmessage=");
-
-			let signature = sanitizeHtml(parts[0].trim(), { allowedTags: [], allowedAttributes: {} });
-			let message   = sanitizeHtml(parts[1].trim(), { allowedTags: [], allowedAttributes: {} });
-
-			if (signature.length == 0)
-				signature = "anonymous";
-
-			if (message.length != 0)			
-				guestbookContent = message + "<br><i> — " + signature + "</i><br><br>\n" + guestbookContent;
-
-			replyWithHomepage(res, guestbookContent);
-
-			fs.writeFileSync(GUESTBOOK_FILEPATH, guestbookContent, "utf-8");
-		});
+		res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+		res.end(fs.readFileSync(HOMEPAGE_FILEPATH, "utf-8"));
 
 	} else {
 
@@ -57,9 +29,3 @@ createServer(options, (req, res) => {
 	}
 
 }).listen(443, "0.0.0.0", () => { console.log(`Starting @ https://dairycultist.dev/`); });
-
-function replyWithHomepage(res, guestbookContent) {
-
-	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-	res.end(fs.readFileSync(HOMEPAGE_FILEPATH, "utf-8").replace("[[[guestbookContent]]]", guestbookContent));
-}
